9594f6f627feab2f4141d2f7e4a7a758bbe45dac
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-08-20 20:33:27 -0500

    begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+    Hero
+    </section>
 
-  export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <section className="padding"> 
+      PopularProducts
+    </section>
+    <section className="padding"> 
+      SuperQuality
+    </section>
+    <section className="padding-x py-10"> 
+      Services
+    </section>
+    <section className="padding"> 
+      SpecialOffers
+    </section>
+    <section className="bg-pale-blue padding"> 
+      CustomReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full"> 
+      Subscribe
+    </section>
+    <section className="padding"> 
+      Footer
+    </section>
+  </main>
+)
 
+
+export default App;
