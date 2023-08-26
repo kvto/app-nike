214da766278636df9dab33df2cfc16398be90d06
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-08-26 13:26:07 -0500

    Creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-const App = () => (
+import Nav from "./components/Nav";
+import { CustomerReviews, Footer, Hero, PopularProducts, Services,
+SpecialOffers, Subscribe, SuperQuality } from "./sections";
+
+const App = () => {
+  return(
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-    Hero
+    <Hero />
     </section>
 
     <section className="padding"> 
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding"> 
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10"> 
-      Services
+      <Services/>
     </section>
     <section className="padding"> 
-      SpecialOffers
+      <SpecialOffers/>
     </section>
     <section className="bg-pale-blue padding"> 
-      CustomReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full"> 
-      Subscribe
+      <Subscribe/>
     </section>
-    <section className="padding"> 
-      Footer
+    <section className="bg-black padding-x padding-t pb-8"> 
+      <Footer/>
     </section>
   </main>
 )
+}
 
 
 export default App;
