eaed0911ee46bc58a76e955ba3dade4d8caa9dce
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-09 16:24:34 -0500

    Shoe card

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ SpecialOffers, Subscribe, SuperQuality } from "./sections";
 
 const App = () => {
   return(
-  <main className="relative">
+    <main className='relative'>
     <Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
-    <Hero />
+    <section className='xl:padding-l wide:padding-r padding-b'>
+      <Hero />
     </section>
-
-    <section className="padding"> 
-      <PopularProducts/>
+    <section className='padding'>
+      <PopularProducts />
     </section>
-    <section className="padding"> 
-      <SuperQuality/>
+    <section className='padding'>
+      <SuperQuality />
     </section>
-    <section className="padding-x py-10"> 
-      <Services/>
+    <section className='padding-x py-10'>
+      <Services />
     </section>
-    <section className="padding"> 
-      <SpecialOffers/>
+    <section className='padding'>
+      <SpecialOffers />
     </section>
-    <section className="bg-pale-blue padding"> 
-      <CustomerReviews/>
+    <section className='bg-pale-blue padding'>
+      <CustomerReviews />
     </section>
-    <section className="padding-x sm:py-32 py-16 w-full"> 
-      <Subscribe/>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe />
     </section>
-    <section className="bg-black padding-x padding-t pb-8"> 
-      <Footer/>
+    <section className=' bg-black padding-x padding-t pb-8'>
+      <Footer />
     </section>
   </main>
 )
